5b8e8be15a96fcaf028decd2b62797f655698657
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-04-27 23:34:34 -0400

    Add background color rotation logic

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,32 @@ $(document).ready(function(){
 
   body.fadeIn();
 
+  backgroundStyleSettings = {
+    colors: [
+      '#2980b9',
+      '#16a085',
+      '#27ae60',
+      '#f39c12',
+      '#d35400',
+      '#c0392b',
+      '#8e44ad'
+    ],
+    currentColorIdx: 0,
+    getColor: function(){
+      if (this.currentColorIdx == this.colors.length - 1) {
+        this.currentColorIdx = 0;
+        return this.colors[this.currentColorIdx];
+      } else {
+        return this.colors[this.currentColorIdx++];
+      }
+    }
+  };
+
+  body.css("background-color", backgroundStyleSettings.getColor());
+  backgroundCycle = setInterval(function(){
+    body.css("background-color", backgroundStyleSettings.getColor());
+  }, 10000);
+
   // Click Events
   startBtn.on("click", function(){
     if (timerStatus) {
