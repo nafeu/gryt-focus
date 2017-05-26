62238865fbe5a3d4c8da161288ab2bb7f70c740b
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-05-26 14:58:09 -0400

    Add script to execute remote commands

---
+++ remote.js
@@ -0,0 +1,42 @@
+var request = require('request');
+
+var appUrl,
+  data,
+  requestUrl,
+  propertiesObject,
+  config;
+
+try {
+  config = require("./config.js");
+}
+catch(err) {
+  console.log("Missing config file, please create config.js following sample-config.js format");
+}
+
+if (config && process.argv.length > 2) {
+
+  appUrl = config.remote_app_url;
+
+  if (appUrl) {
+
+    requestUrl = appUrl + "/api/interact";
+    propertiesObject = {};
+
+    propertiesObject.action = process.argv[2];
+    if (process.argv[3]) propertiesObject.data = process.argv[3];
+
+    console.log("Making request to: " + requestUrl);
+    console.log("With query vars: ", propertiesObject);
+
+    request({url:requestUrl, qs:propertiesObject}, function(err, response, body) {
+      if(err) { console.log(err); return; }
+      console.log("Response: " + response);
+    });
+
+  } else {
+    console.log("Missing appUrl in config.js");
+  }
+
+} else {
+  console.log("Invalid arguments");
+}
