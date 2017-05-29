fc2b02ff15ba497b0dc7a49cc0cb53505382fa10
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-05-29 16:37:05 -0400

    Update kiosk script to run webserver

--- runkiosk.sh
+++ runkiosk.sh
@@ -4,5 +4,11 @@
 #          Run chrome kiosk for rpi
 # ------------------------------------------------------------------
 
-export DISPLAY=:0.0
-chromium-browser --kiosk http://localhost:8000?remote=true
+activate_kiosk(){
+  sleep 5
+  export DISPLAY=:0.0
+  chromium-browser --kiosk http://localhost:8000?remote=true
+}
+
+activate_kiosk &
+node server.js
