57ec18ad94a3ca09164dfc48e4807d0ab3c35b96
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-06-20 04:02:02 -0400

    Update kiosk script to prevent new app instance on every login

--- runkiosk.sh
+++ runkiosk.sh
@@ -4,13 +4,15 @@
 #          Run chrome kiosk for rpi
 # ------------------------------------------------------------------
 
+BASEDIR=$(dirname "$0")
+
 activate_kiosk(){
   sleep 5
   export DISPLAY=:0.0
   chromium-browser --kiosk http://localhost:8000?remote=true
 }
 
-BASEDIR=$(dirname "$0")
-
-activate_kiosk &
-node $BASEDIR/server.js
+if [ -z "$SSH_CLIENT" ] && [ -z "$SSH_TTY" ]; then
+  activate_kiosk &
+  node "$BASEDIR/server.js"
+fi
