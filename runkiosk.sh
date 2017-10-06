aa159e8b4705b1054d1d1d9296d2819889dca3da
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-10-06 00:12:33 -0400

    Update kiosk script to contain example query param options

--- runkiosk.sh
+++ runkiosk.sh
@@ -10,7 +10,7 @@ activate_kiosk(){
   sleep 5
   export DISPLAY=:0.0
   chromium-browser --kiosk http://localhost:8000?remote=true
-  # chromium-browser --kiosk http://localhost:8000?remote=true&theme=%231C2021,%23E66A39,%231C2021,%23D04E33
+  # chromium-browser --kiosk http://localhost:8000?remote=true&theme=%231C2021&mode=day
 }
 
 if [ -z "$SSH_CLIENT" ] && [ -z "$SSH_TTY" ]; then
