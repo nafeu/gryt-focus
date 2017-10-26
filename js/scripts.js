9776e363bebf52607f13ba245bac51f64261d762
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-10-26 16:27:24 -0400

    Remove chart debug code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-  document.addEventListener('keypress', function(event) {
-    if (event.which == 114) {
-      updateData(0.5);
-      renderChart();
-    }
-  });
-
   function main() {
     contentTask.css('font-size', DEFAULT_TEXTAREA_FONT_SIZE);
     body.fadeIn();
